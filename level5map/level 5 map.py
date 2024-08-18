029509ba62c663661e5c5608bc92eeee614a0b28
Taraquinzel0505 <taraquinzel@gmail.com>
2024-08-18 12:12:34 +0800

    Add files via upload

---
+++ level5map/level 5 map.py
@@ -0,0 +1,7 @@
+import pygame
+import sys
+def level_5(choice):
+    choice_player = input("click start to begin level 5")
+    if choice_player = ("click start")
+print("Prepare to enter Asgard")
+
