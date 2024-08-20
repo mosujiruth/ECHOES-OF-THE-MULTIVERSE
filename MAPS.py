15fd6f2c45ccb6859568fe1e4ef0833441e0f0b7
mosujiruth <sujiruth07@gmail.com>
2024-08-20 13:53:59 +0800

    Initialise repository

--- MAPS.py
+++ MAPS.py
@@ -1,50 +1,48 @@
 import pygame
 import sys
 
-pygame.init
- #colour
+pygame.init()
+pygame.font.init()
+
 BLACK=(255,255,255)
 WHITE=(0,0,0)
 
-#screen setup
-screen_width=600
-screen_height=400
-screen=pygame.display.set_mode((screen_width,screen_height))
+# will display the screen 
+screen_length = 600
+screen_height = 400
+window = pygame.display.set_mode((screen_length, screen_height))
 pygame.display.set_caption("ECHOES OF THE MULTIVERSE")
+font = pygame.font.SysFont('Comic Sans', 48)
 
-# Load your image
-background_image = pygame.image.load('game bg.webp')
+#display image
+image=pygame.image.load("game_bg.webp")
+#please display the image
+image=pygame.transform.scale(image,(screen_length,screen_height))
 
-# Reduce the size (e.g., half the original size)
-new_width = background_image.get_width() // 2
-new_height = background_image.get_height() // 2
+#start button
+start_img=pygame.image.load("start.png").convert_alpha()
+start_img=pygame.transform.scale(start_img,(130,130))
 
-# Resize the image
-background_image = pygame.transform.scale(background_image, (new_width, new_height))
+class button:
+    def __init__(self, x, y):
+        self.image=start_img
+        self.rect=self.image.get_rect()
+        self.rect.topleft=(x,y)
+    def draw(self,surface):
+        surface.blit(self.image,(self.rect.x,self.rect.y))    
 
+start_button =button(240, 140)
+      
 
-# Set up the display
-screen = pygame.display.set_mode((background_image.get_width(), background_image.get_height()))
-pygame.display.set_caption('ECHOES OF THE MULTIVERSE')
-
-
-
-# Main loop
+# loop 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-
-    # Draw the background image
-    screen.blit(background_image, (0, 0))
-
-    # Update the display
+            running = False     
+    window.blit(image,(0,0))
+    start_button.draw(window)
     pygame.display.flip()
-
-# Quit pygame
+ 
 pygame.quit()
 sys.exit
-
-
-
