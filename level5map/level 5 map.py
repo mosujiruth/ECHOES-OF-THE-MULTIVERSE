d83e6e7f9907c68200a7ff2145f1981a18fb6f74
Taraquinzel0505 <taraquinzel@gmail.com>
2024-08-20 09:55:25 +0800

    Update level 5 map.py

--- level5map/level 5 map.py
+++ level5map/level 5 map.py
@@ -1,7 +1,8 @@
-# Tears of Tarshni
+# Tears of Tarshni muhuthan
 import pygame
 import sys
 pygame.init()
+#display
 width, height =800, 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('asgard.jpg')
@@ -9,23 +10,26 @@ pygame.display.set_caption('asgard.jpg')
 
 WELCOME_SCREEN = "Welcome_to_asgard"
 TREASURE_ROOM = "treasure_room"
-PUZZLE_SOLVED = "puzzle_solved"
+PUZZLE_COMPLETE = "puzzle_complete"
 NEXT_LEVEL = "next_level"
 def main():
     current_state = "Welcome_to_asgard"
     running = True
-
     while running:
+        for event in pygame.event.get():  # This is the event loop inside the main loop
+            if event.type == pygame.QUIT:
+                running = False
         if current_state == WELCOME_SCREEN:
-          print("Welcome to Asgard! Press any key to use reality stone and enter the treasure room.")
+          pygame.display.set_caption("Welcome to Asgard! Press any key to use reality stone and enter the treasure room.")
         elif current_state == TREASURE_ROOM:
-            print("You are now in the treasure room. Solve the puzzle to get the Space Stone.")
+            pygame.display.set_caption("You are now in the treasure room. Solve the puzzle to get the Space Stone.")
             #will code for 4 jigzaw puzzle later
            #display after puzzle is complete
-        elif current_state == PUZZLE_SOLVED:
-           print("Great job soldier! your one step closer in defeating TITAN Press any key to proceed.")
+        elif current_state == PUZZLE_COMPLETE:
+          pygame.display.set_caption("Great job soldier! your one step closer in defeating TITAN Press any key to proceed.")
+           #Will merge with upcoming level
         elif current_state == NEXT_LEVEL:
-            print("level 6")
+            pygame.display.set_caption("level 6")
             pygame.display.flip()
             pygame.time.delay(3000)  # this will show for 3 sec
             pygame.quit()
@@ -34,15 +38,16 @@ def main():
 
         if current_state != "Welcome_to_asgard":
 
-    pygame.quit()
+            pygame.quit()
     sys.exit()
 
 if __name__ == "__main__":
     main()
 
 
+
+
     
-    pygame.quit()
-    sys.exit()
+    
 
 
