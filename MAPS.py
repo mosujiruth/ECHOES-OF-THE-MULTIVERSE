56d2a64c95afbf53653ac0e5204f4b84cace1068
mosujiruth <sujiruth07@gmail.com>
2024-08-21 10:07:01 +0800

    Initialise repository

--- MAPS.py
+++ MAPS.py
@@ -23,6 +23,7 @@ image=pygame.transform.scale(image,(screen_length,screen_height))
 start_img=pygame.image.load("start.png").convert_alpha()
 start_img=pygame.transform.scale(start_img,(130,130))
 
+
 class button:
     def __init__(self, x, y):
         self.image=start_img
@@ -31,18 +32,39 @@ class button:
     def draw(self,surface):
         surface.blit(self.image,(self.rect.x,self.rect.y))    
 
+#button for all
 start_button =button(240, 140)
-      
+
+
+current_page=0
+chossing_character=1
+current_level=current_page
 
 # loop 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False     
-    window.blit(image,(0,0))
-    start_button.draw(window)
+            running = False      
+    if event.type == pygame.MOUSEBUTTONDOWN:
+            if current_level ==  current_page: 
+                print("Start Button Clicked!")
+                current_level = chossing_character 
+   
+    if current_level ==current_page :
+        window.blit(image, (0, 0))
+        start_button.draw(window)
+    elif current_level == chossing_character: 
+        window.blit(image,(0,0))
+   
     pygame.display.flip()
+
+def chossing_character():
+    window.blit(image,(0,0))
+    pygame.display.flip()
+    
+   
+
  
 pygame.quit()
 sys.exit
